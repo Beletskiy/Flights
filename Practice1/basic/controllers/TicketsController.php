adeec08701536a8b08d603707edaecb91c0ca36d
Beletskiy <Alexey1Beletskiy@gmail.com>
2015-06-04 10:08:44 +0300

    Revert "4_06_2015"
    This reverts commit 8616d17401c02286498bf0041524e7cfdce60d46.

--- Practice1/basic/controllers/TicketsController.php
+++ Practice1/basic/controllers/TicketsController.php
@@ -11,25 +11,21 @@ class TicketsController extends \yii\web\Controller
         $flights = new \app\models\Flights();
         $passengers = new \app\models\Passengers();
        
-   
+    /*    return $this->render('index', [
+                'model' => $model,
+                'flights' => $flights,
+                'passengers' =>$passengers
+            ]); */
       //  $model->attributes = \Yii::$app->request->post();
         
         $passengers->load(Yii::$app->request->post());
         Yii::trace($passengers->attributes);
-        Yii::trace($passengers->age);
-        $passengers->save($runValidation = false);
         
-        $model->load(Yii::$app->request->post());
-        $model->passenger_id = $passengers->id;
-        $model->date = $this->date;
-        $model->save($runValidation = false);
-        
-     /*   if ($model->load(Yii::$app->request->post())) {
+        if ($model->load(Yii::$app->request->post())) {
             if ($model->signup()) {
                     return $this->goHome();
             }
-        } */
-        
+        }
         return $this->render('index', [
             'model' => $model,
             'flights' => $flights,
