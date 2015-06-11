c5fb199322a2e99415eefe88dff49e11cbadb8bf
Beletskiy <Alexey1Beletskiy@gmail.com>
2015-06-11 10:15:45 +0300

    11_06_2015

--- Practice1/basic/views/site/index.php
+++ Practice1/basic/views/site/index.php
@@ -5,10 +5,9 @@ $this->title = 'Flights';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Авиакомпания</h1><br>
+        <h1 style="color:white">Авиакомпания</h1><br>
         
         <p><a class="btn btn-lg btn-success" href="http://localhost/beletskiy/Yii_Task2/Practice1/basic/web/index.php?r=flights/index">Просмотр/редактирование авиарейсов</a></p>
-        <p><a class="btn btn-lg btn-success" href="http://localhost/beletskiy/Yii_Task2/Practice1/basic/web/index.php?r=place/index">Просмотр/редактирование посадочных мест</a></p>
         <p><a class="btn btn-lg btn-success" href="http://localhost/beletskiy/Yii_Task2/Practice1/basic/web/index.php?r=tickets/index">Заказ билетов</a></p>
     </div>
 
